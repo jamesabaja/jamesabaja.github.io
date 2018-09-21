462372a361ba8d0420a36406d5e7c8ab770d5695
jgabaja <jgabaja@gmail.com>
2018-09-21 13:45:23 +0800

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Card, Icon} from 'semantic-ui-react';
 
 const Contact = () => {
   return(
     <div className='main-content'>
       <div className='ui center aligned container'>
         <h1>Contact Me</h1>
-        <p className='larger'>This site is still a WIP (work in progress)! Please bear with me :)</p>
+        <Card centered>
+          <Card.Content>
+            <Icon name='mail' /> jgabaja@gmail.com
+          </Card.Content>
+          <Card.Content>
+            <Icon name='facebook' /> <a href='http://www.facebook.com/jgabaja' target='_blank' rel='noopener noreferrer'>@jgabaja</a>
+          </Card.Content>
+          <Card.Content>
+            <Icon name='twitter' /> <a href='https://www.twitter.com/jmsgbrl__' target='_blank' rel='noreferrer noopener'>@jmsgbrl__</a> 
+          </Card.Content>
+          <Card.Content>
+            <Icon name='instagram' /> <a href='https://www.instagram.com/jmsgbrl__'target='_blank' rel='noreferrer noopener'>@jmsgbrl__</a>
+          </Card.Content>
+          <Card.Content>
+            <Icon name='github' /> <a href='https://www.github.com/jamesabaja'target='_blank' rel='noreferrer noopener'>@jamesabaja</a>
+          </Card.Content>
+        </Card>
       </div>
     </div>
   );
